b71fccf854b8da38411edd505233f6bfda85468c
pawciogaa <pawciogaa@gmail.com>
2024-08-12 16:50:41 +0200

    progresss

--- GRAPH_ALGORITHMS/Dijkstra.py
+++ GRAPH_ALGORITHMS/Dijkstra.py
@@ -4,14 +4,18 @@ def Dijskra(G,s):
     n = len(G)
     parents = [None for i in range(n)]
     weights = [inf for i in range(n)]
+    done = [False for i in range(n)]
     pq = PriorityQueue()
     pq.put((0,s))
+    weights[s] = 0
 
     while not pq.empty():
         _,u = pq.get()
-        for v,weight in G[u]:
-            if weights[v] > weights[u] + weight:
-                weights[v] = weights[u] + weight
-                parents[v] = u
-                pq.put((weights[v],v))
+        if not done[u]:
+            for v,weight in G[u]:
+                if weights[v] > weights[u] + weight:
+                    weights[v] = weights[u] + weight
+                    parents[v] = u
+                    pq.put((weights[v],v))
+        done[u] = True
     return parents, weights
